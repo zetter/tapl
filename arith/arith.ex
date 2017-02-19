c8a6ac2de8582ad3bd6e13b5b9bd8d307b13e723
Chris Zetter <zetter@gmail.com>
2017-02-19 12:29:42 +0000

    Match terminology of the book

--- arith/arith.ex
+++ arith/arith.ex
@@ -3,24 +3,24 @@ defmodule Arith do
     defexception message: "No rule applies"
   end
 
-  def evaluate(term) do
+  def eval(term) do
     try do
-      evaluate(evaluate1(term))
+      eval(eval1(term))
     rescue
       NoRuleApplies -> term
     end
   end
 
-  def evaluate1({:pred, term}) do
-    evaluate1({:pred, term}, is_numeric?(term))
+  def eval1({:pred, term}) do
+    eval1({:pred, term}, is_numeric?(term))
   end
 
-  def evaluate1({:is_zero, term}) do
-    evaluate1({:is_zero, term}, is_numeric?(term))
+  def eval1({:is_zero, term}) do
+    eval1({:is_zero, term}, is_numeric?(term))
   end
 
-  def evaluate1(term) do
-    evaluate1(term, false)
+  def eval1(term) do
+    eval1(term, false)
   end
 
   defp is_numeric?(:zero) do
@@ -35,47 +35,47 @@ defmodule Arith do
     false
   end
 
-  defp evaluate1({:if, :true, branch, _}, _) do
+  defp eval1({:if, :true, branch, _}, _) do
     branch
   end
 
-  defp evaluate1({:if, :false, _, branch}, _) do
+  defp eval1({:if, :false, _, branch}, _) do
     branch
   end
 
-  defp evaluate1({:if, conditional, branch_1, branch_2}, _) do
-    {:if, evaluate1(conditional), branch_1, branch_2}
+  defp eval1({:if, conditional, branch_1, branch_2}, _) do
+    {:if, eval1(conditional), branch_1, branch_2}
   end
 
-  defp evaluate1({:succ, term}, _) do
-    {:succ, evaluate1(term)}
+  defp eval1({:succ, term}, _) do
+    {:succ, eval1(term)}
   end
 
-  defp evaluate1({:pred, :zero}, _) do
+  defp eval1({:pred, :zero}, _) do
     :zero
   end
 
-  defp evaluate1({:pred, {:succ, term}}, true) do
+  defp eval1({:pred, {:succ, term}}, true) do
     term
   end
 
-  defp evaluate1({:pred, term}, _) do
-    {:pred, evaluate1(term)}
+  defp eval1({:pred, term}, _) do
+    {:pred, eval1(term)}
   end
 
-  defp evaluate1({:is_zero, :zero}, _) do
+  defp eval1({:is_zero, :zero}, _) do
     :true
   end
 
-  defp evaluate1({:is_zero, {:succ, _}}, true) do
+  defp eval1({:is_zero, {:succ, _}}, true) do
     :false
   end
 
-  defp evaluate1({:is_zero, term}, _) do
-    {:is_zero, evaluate1(term)}
+  defp eval1({:is_zero, term}, _) do
+    {:is_zero, eval1(term)}
   end
 
-  defp evaluate1(_, _) do
+  defp eval1(_, _) do
     raise NoRuleApplies
   end
 end
--- arith/arith_test.exs
+++ arith/arith_test.exs
@@ -5,32 +5,32 @@ defmodule ArithTest do
   ExUnit.start(timeout: 100)
 
   test 'values' do
-    assert Arith.evaluate(:true) == :true
-    assert Arith.evaluate(:false) == :false
-    assert Arith.evaluate(:zero) == :zero
+    assert Arith.eval(:true) == :true
+    assert Arith.eval(:false) == :false
+    assert Arith.eval(:zero) == :zero
   end
 
   test 'conditionals' do
-    assert Arith.evaluate({:if, :true, :true, :false}) == :true
-    assert Arith.evaluate({:if, :false, :true, :false}) == :false
-    assert Arith.evaluate({:if, {:if, :true, :true, :false}, :true, :false}) == :true
-    assert Arith.evaluate({:if, {:if, :false, :true, :false}, :true, :false}) == :false
-    assert Arith.evaluate({:if, :true, {:if, :true, :true, :true}, :false}) == :true
-    assert Arith.evaluate({:if, :false, :true, {:if, :true, :false, :false}}) == :false
+    assert Arith.eval({:if, :true, :true, :false}) == :true
+    assert Arith.eval({:if, :false, :true, :false}) == :false
+    assert Arith.eval({:if, {:if, :true, :true, :false}, :true, :false}) == :true
+    assert Arith.eval({:if, {:if, :false, :true, :false}, :true, :false}) == :false
+    assert Arith.eval({:if, :true, {:if, :true, :true, :true}, :false}) == :true
+    assert Arith.eval({:if, :false, :true, {:if, :true, :false, :false}}) == :false
   end
 
   test 'pred and succ' do
-    assert Arith.evaluate({:succ, :zero}) == {:succ, :zero}
-    assert Arith.evaluate({:pred, :zero}) == :zero
-    assert Arith.evaluate({:succ, {:pred, :zero}}) == {:succ, :zero}
-    assert Arith.evaluate({:pred, {:succ, :zero}}) == :zero
-    assert Arith.evaluate({:pred, {:succ, :true}}) == {:pred, {:succ, :true}}
+    assert Arith.eval({:succ, :zero}) == {:succ, :zero}
+    assert Arith.eval({:pred, :zero}) == :zero
+    assert Arith.eval({:succ, {:pred, :zero}}) == {:succ, :zero}
+    assert Arith.eval({:pred, {:succ, :zero}}) == :zero
+    assert Arith.eval({:pred, {:succ, :true}}) == {:pred, {:succ, :true}}
   end
 
   test 'is zero' do
-    assert Arith.evaluate({:is_zero, :zero}) == :true
-    assert Arith.evaluate({:is_zero, {:pred, :zero}}) == :true
-    assert Arith.evaluate({:is_zero, {:succ, :zero}}) == :false
-    assert Arith.evaluate({:is_zero, {:succ, :true}}) == {:is_zero, {:succ, :true}}
+    assert Arith.eval({:is_zero, :zero}) == :true
+    assert Arith.eval({:is_zero, {:pred, :zero}}) == :true
+    assert Arith.eval({:is_zero, {:succ, :zero}}) == :false
+    assert Arith.eval({:is_zero, {:succ, :true}}) == {:is_zero, {:succ, :true}}
   end
 end
