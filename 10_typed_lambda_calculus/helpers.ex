d0d55642f2718bed2ba9f8143ef29b0137149d48
Chris Zetter <zetter@gmail.com>
2017-05-22 19:08:15 +0100

    Convert tests to use more helpers
    Hope this will mean safer constructions

--- 10_typed_lambda_calculus/helpers.ex
+++ 10_typed_lambda_calculus/helpers.ex
@@ -10,4 +10,24 @@ defmodule Helpers do
   def app(t1, t2) do
     {:app, t1, t2}
   end
+
+  def tm_if(t1, t2, t3) do
+    {:if, t1, t2, t3}
+  end
+
+  def type_arr(type_1, type_2) do
+    {:type_arr, type_1, type_2}
+  end
+
+  def type_bool do
+    :type_bool
+  end
+
+  def tm_true do
+    :true
+  end
+
+  def tm_false do
+    :true
+  end
 end
--- 10_typed_lambda_calculus/lambda_calculus_test.exs
+++ 10_typed_lambda_calculus/lambda_calculus_test.exs
@@ -7,29 +7,29 @@ defmodule LambdaCalculusTest do
   ExUnit.start(timeout: 100)
 
   test 'types when using abs and app' do
-    bool_to_bool = abs("n", :type_bool, var(0))
-    assert LambdaCalculus.type_of(bool_to_bool) == {:type_arr, :type_bool, :type_bool}
-    assert LambdaCalculus.type_of(app(abs("n", :type_bool, var(0)), :true)) == :type_bool
+    bool_to_bool = abs("n", type_bool(), var(0))
+    assert LambdaCalculus.type_of(bool_to_bool) == type_arr(type_bool(), type_bool())
+    assert LambdaCalculus.type_of(app(abs("n", type_bool(), var(0)), tm_true())) == type_bool()
 
     assert_raise RuntimeError, "parameter type mismatch", fn ->
-      LambdaCalculus.type_of(app(abs("n", :type_bool, var(0)), bool_to_bool))
+      LambdaCalculus.type_of(app(abs("n", type_bool(), var(0)), bool_to_bool))
     end
   end
 
   test 'types when using true and false' do
-    assert LambdaCalculus.type_of(:true) == :type_bool
-    assert LambdaCalculus.type_of(:false) == :type_bool
-    assert LambdaCalculus.type_of({:if, :true, :true, :false}) == :type_bool
+    assert LambdaCalculus.type_of(tm_true()) == type_bool()
+    assert LambdaCalculus.type_of(tm_true()) == type_bool()
+    assert LambdaCalculus.type_of(tm_if(tm_true(), tm_true(), tm_false())) == type_bool()
 
-    bool_to_bool = abs("n", :type_bool, var(0))
-    assert LambdaCalculus.type_of({:if, :true, bool_to_bool, bool_to_bool}) == {:type_arr, :type_bool, :type_bool}
+    bool_to_bool = abs("n", type_bool(), var(0))
+    assert LambdaCalculus.type_of(tm_if(tm_true(), bool_to_bool, bool_to_bool)) == type_arr(type_bool(), type_bool())
 
     assert_raise RuntimeError, "arms of conditional have different types", fn ->
-      LambdaCalculus.type_of({:if, :true, :false, bool_to_bool})
+      LambdaCalculus.type_of(tm_if(tm_false(), tm_true(), bool_to_bool))
     end
 
     assert_raise RuntimeError, "guard of conditioanl not a boolean", fn ->
-      LambdaCalculus.type_of({:if, bool_to_bool, :false, :false})
+      LambdaCalculus.type_of(tm_if(bool_to_bool, tm_false(), tm_false()))
     end
   end
 
